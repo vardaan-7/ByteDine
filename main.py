6af1e309e5de158044ef0184760d4a98b853cbfa
vardaan-7 <vardaan.shukla7@gmail.com>
2025-05-17 04:37:25 +0530

    integrate classes

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ from app.user import User
 from app.restaurant import Restaurant
 from app.menu import MenuItem
 from app.deliveryboy import DeliveryBoy
-
-users = []
-restaurants = []
-deliveryboys = []
+from app.order import Order
 
 r1 = Restaurant("Pizza Palace", "Noida")
-r1.add_menu_item("Pepperoni Pizza", 299)
-r1.add_menu_item("Veggie Pizza", 249)
-restaurants.append(r1)
+r1.add_menu_item(MenuItem("Pepperoni Pizza", 299))
+r1.add_menu_item(MenuItem("Veggie Pizza", 249))
 
 # Create users
 u1 = User("Riya")
-users.append(u1)
-
-# Create delivery boys
 d1 = DeliveryBoy("Aman")
-deliveryboys.append(d1)
+
+r1.view_menu()
+order = u1.place_order(r1,"Veggie Pizza")
+
+if order:
+    d1.assign_order(order)
+
+u1.view_order_history()
